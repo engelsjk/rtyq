b2381882f20834d73fd85edac1f0649a1560d211
engelsjk <jon.engelsman@gmail.com>
2021-12-14 16:16:23 -0800

    update version

--- conf/appconfig.go
+++ conf/appconfig.go
@@ -9,5 +9,5 @@ type AppConfiguration struct {
 var AppConfig = AppConfiguration{
 	Name:    "rtyq",
 	Help:    "generate and query in-memory spatial rtree",
-	Version: "0.2.0",
+	Version: "0.2.1",
 }
